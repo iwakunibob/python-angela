7264851109a84cf040017f955ff9dd9880a2df90
iwakunibob <rml128+github@gmail.com>
2025-02-13 00:51:29 +1000

    orig

---
+++ 01-types/types.py
@@ -0,0 +1,9 @@
+print(type("Hello"))
+print(type(12))
+print(type(3.14))
+print(type(True))
+
+print("123" + "456")
+print(int("123") + int("456"))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
